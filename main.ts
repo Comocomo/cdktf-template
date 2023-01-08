52b3a6efb2d03f87f37378bc73984507a4bf4c3a
shira-lev <shiralev84@gmail.com>
2023-01-08 09:12:44 +0200

    template files

--- main.ts
+++ main.ts
@@ -2,22 +2,18 @@
 // SPDX-License-Identifier: MPL-2.0
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
-import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { NutrinoCdktfStack } from "nutrino-tf/NutrinoCdktfStack";
 // Import modules here  
 // import { ModuleName } from "./.gen/modules/<ModuleName>"
 // The ModuleName is the name you set to it in the cdktf.json with a capital first letter
 
-class MyStack extends TerraformStack {
+class MyStack extends NutrinoCdktfStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
-    
-    new AwsProvider(this, "AWS", {
-      region: "Enter region here"
-    })
     // define resources here
   }
 }
 
 const app = new App();
-new MyStack(app, "cdktf_test");
+new MyStack(app, "cdktf_serverless_app");
 app.synth();
