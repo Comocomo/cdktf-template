804fa497b4f396440121646a3b2a248ce16ee245
shira-lev <shiralev84@gmail.com>
2023-01-19 14:57:08 +0200

    adding repo name

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MPL-2.0
 import { Construct } from "constructs";
 import { App } from "cdktf";
-import { NutrinoCdktfStack } from "nutrino-tf/NutrinoCdktfStack";
+import { NutrinoCdktfStack , repoName } from "nutrino-tf/NutrinoCdktfStack";
 // Import modules here  
 // import { ModuleName } from "./.gen/modules/<ModuleName>"
 // The ModuleName is the name you set to it in the cdktf.json with a capital first letter
