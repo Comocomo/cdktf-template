0feac70c1be249ae8496ff7e703afaebfb7f3002
shira-lev <shiralev84@gmail.com>
2023-01-23 11:22:42 +0200

    adding init change

--- main.ts
+++ main.ts
@@ -2,15 +2,21 @@
 // SPDX-License-Identifier: MPL-2.0
 import { Construct } from "constructs";
 import { App } from "cdktf";
+import simpleGit from 'simple-git';
 import { NutrinoCdktfStack , repoName } from "nutrino-tf/NutrinoCdktfStack";
 import { NutrinoTFOutput } from "nutrino-tf/NutrinoTFOutput";
 // Import modules here  
 // import { ModuleName } from "./.gen/modules/<ModuleName>"
 // The ModuleName is the name you set to it in the cdktf.json with a capital first letter
 
+async function getGitRootDir() {
+    const git = simpleGit(process.cwd()) as any;
+    return await git.revparse('--show-toplevel')
+}
+
 class MyStack extends NutrinoCdktfStack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+  async init() {
+        await super.init()
     // define resources here
 
 
@@ -18,6 +24,9 @@ class MyStack extends NutrinoCdktfStack {
   }
 }
 
-const app = new App();
-new MyStack(app, `${repoName}-infra`);
-app.synth();
+(async() => {
+    const app = new App();
+    const stack = new MyStack(app, getGitRootDir());
+    await stack.init();
+    app.synth();
+})()
