e83f373c14726403ca9df39584b4a099fa4cd247
shira-lev <shiralev84@gmail.com>
2023-02-12 13:59:57 +0200

    updated template with getRepo function

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MPL-2.0
 import { Construct } from "constructs";
 import { App } from "cdktf";
-import { NutrinoCdktfStack , repoName, NutrinoTFOutput } from "@comocomo/cdktf-nutrino-class";
+import { NutrinoCdktfStack , getRepo, NutrinoTFOutput } from "@comocomo/cdktf-nutrino-class";
 // Import modules here  
 // import { ModuleName } from "./.gen/modules/<ModuleName>"
 // The ModuleName is the name you set to it in the cdktf.json with a capital first letter
@@ -19,6 +19,11 @@ class MyStack extends NutrinoCdktfStack {
   }
 }
 
-const app = new App();
-new MyStack(app, `${repoName}-infra`);
-app.synth();
+async function main() {
+  const app = new App();
+  const name = await getRepo();
+  new MyStack(app, name);
+  app.synth();
+}
+
+main();
