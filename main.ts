070e9a5f2805ce15d973943a447991d9ad71649a
shira-lev <shiralev84@gmail.com>
2023-03-22 11:58:50 +0200

    new template with abstract class

--- main.ts
+++ main.ts
@@ -1,29 +1,21 @@
 // Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
 import { Construct } from "constructs";
-import { App } from "cdktf";
-import { NutrinoCdktfStack , getRepo, NutrinoTFOutput } from "@comocomo/cdktf-nutrino-class";
-// Import modules here  
+import { NutrinoCdktfStack, NutrinoTFOutput, apply} from "@comocomo/cdktf-nutrino-class";
+// Import modules here
 // import { ModuleName } from "./.gen/modules/<ModuleName>"
 // The ModuleName is the name you set to it in the cdktf.json with a capital first letter
 // When using the NutrinoTFOutput the repo param should be this.getId() - `repo: this.getId()`
 class MyStack extends NutrinoCdktfStack {
-    constructor(scope: Construct, id: string){
-        super(scope, id);
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+  }
 
+  async myStackInit() {
     // define resources here
 
-
     // define outputs here
-
   }
 }
 
-async function main() {
-  const app = new App();
-  const name = await getRepo();
-  new MyStack(app, name);
-  app.synth();
-}
-
-main();
+apply(MyStack);
