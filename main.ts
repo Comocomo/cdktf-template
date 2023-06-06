08dcdecc794e4c17ace908b44a427fc9b04b6f20
nadavbrkt <nadav.brkt@gmail.com>
2023-06-06 15:19:29 +0300

    fix: add EnvData

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 // Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
 import { Construct } from "constructs";
-import { NutrinoCdktfStack, NutrinoTFOutput, apply} from "@comocomo/cdktf-nutrino-class";
+import { NutrinoCdktfStack, NutrinoTFOutput, EnvData, apply} from "@comocomo/cdktf-nutrino-class";
 // Import modules here
 // import { ModuleName } from "./.gen/modules/<ModuleName>"
 // The ModuleName is the name you set to it in the cdktf.json with a capital first letter
 // When using the NutrinoTFOutput the repo param should be this.getId() - `repo: this.getId()`
 class MyStack extends NutrinoCdktfStack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+  constructor(scope: Construct, id: string, env: EnvData) {
+    super(scope, id, env);
   }
 
   async myStackInit() {
