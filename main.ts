15e499658a835fd92878d737cc09282cfe3029dd
shira-lev <shiralev84@gmail.com>
2023-06-11 16:48:33 +0300

    remove old comment

--- main.ts
+++ main.ts
@@ -5,7 +5,6 @@ import { NutrinoCdktfStack, NutrinoTFOutput, EnvData, apply} from "@comocomo/cdk
 // Import modules here
 // import { ModuleName } from "./.gen/modules/<ModuleName>"
 // The ModuleName is the name you set to it in the cdktf.json with a capital first letter
-// When using the NutrinoTFOutput the repo param should be this.getId() - `repo: this.getId()`
 class MyStack extends NutrinoCdktfStack {
   constructor(scope: Construct, id: string, env: EnvData) {
     super(scope, id, env);
